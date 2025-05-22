0eee2123a7420aed4f4bbd0f1739a7b9d7ee5ebd
aayalac27 <aayalac@eafit.edu.co>
2025-05-22 12:33:27 -0500

    Update botones.py

--- pages/botones.py
+++ pages/botones.py
@@ -55,19 +55,4 @@ if st.button('OFF'):
     ret= client1.publish("AlejoCerradura", message)
   
     
-else:
-    st.write('')
 
-values = st.slider('Selecciona el rango de valores',0.0, 100.0)
-st.write('Values:', values)
-
-if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
-    client1.on_publish = on_publish                          
-    client1.connect(broker,port)   
-    message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("cmqtt_a", message)
-    
- 
-else:
-    st.write('')
